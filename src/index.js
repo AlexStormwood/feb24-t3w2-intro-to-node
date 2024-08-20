0b812b553eda41b315d3b274f911d557929c4048
Alex <alex.holder@bigfootds.com>
2024-08-20 19:48:39 +1000

    feat(dice): rollDice implemented

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log("Dice roller app is now running!");
+
+
+/**
+ * Generate a random number between 1 and diceSize, inclusive. 
+ * @param {number} diceSize The maximum that the dice can roll.
+ * @returns {number} The number rolled on the dice.
+ */
+function rollDice(diceSize = 20){
+	let diceResult = 0;
+
+	
+	diceResult = Math.ceil(Math.random() * diceSize);
+
+	return diceResult;
+}
+
+
+console.log("Random dice roll result is: " + rollDice());
